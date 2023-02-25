dc2a371a9b04a323f31ac2ee03901ff8e49cf0f8
Sellsword9 <yerayromeroortiz@gmail.com>
2023-02-25 01:30:28 +0100

    v0.0101+

---
+++ CShardFiles/Baraja.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+public class Baraja
+{
+    List<Carta> Cartas;
+    public Baraja() //Crea una baraja ordenada 
+    {
+        for (int a = 1; a<52; a++)
+        {
+            Cartas.Add(new Carta(a));
+        }
+    }
+
+    public static Baraja getRandom() //Crea y devuelve una baraja randomizada
+    {
+        return new Baraja().mezclar();
+    }
+
+    public Baraja mezclar()
+    {
+        int a;
+        int b;
+        Random r = new Random();
+        for (int i = 0; i < 10000; i++)
+            {
+            do //Solo para asegurarse
+            {
+            a = r.Next(0, 53);
+            } while (a > 52 || a < 1);
+
+            do //Solo para asegurarse
+            {
+            b = r.Next(0, 53);
+            } while (b > 52 || b < 1);
+            
+            Carta temp = this.Cartas[a];
+            this.Cartas[a] = this.Cartas[b];
+            this.Cartas[b] = temp;
+            }
+
+        return this;
+
+        
+    }
+
+    public List<Carta> getPrimerasCartas(int num)
+    {
+        List<Carta> cartasReturn = new List<Carta>();
+        for (int i = 0; i<num; i++)
+        {
+            cartasReturn.Add(Cartas[i]);
+        }
+        return cartasReturn;
+    }
+}
---
+++ CShardFiles/PlayerManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Este script debe poner el dinero del jugador en su stock y viceversa
+    public static int DineroJugador;
+}
