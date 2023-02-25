d2c6c72956832961c8fbdd464c043ade68395ad4
Yeray Romero <118433333+Sellsword9@users.noreply.github.com>
2023-02-25 01:51:08 +0100

    Delete PlayerManager.cs

--- PlayerManager.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerManager : MonoBehaviour
-{
-    // Este script debe poner el dinero del jugador en su stock y viceversa
-    public static int DineroJugador;
-}
